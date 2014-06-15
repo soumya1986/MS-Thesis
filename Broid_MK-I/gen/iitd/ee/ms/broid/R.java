52f4cfb836225c40aef0f89638667df013d53aa5
soumya1986 <soumyasengupta1986@gmail.com>
2014-06-15 15:28:50 +0530

    Made the bare bones Phone, message and music activity with buttons

--- Broid_MK-I/gen/iitd/ee/ms/broid/R.java
+++ Broid_MK-I/gen/iitd/ee/ms/broid/R.java
@@ -984,6 +984,7 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020057;
     }
     public static final class id {
+        public static final int Button02=0x7f05003e;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -995,7 +996,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f050040;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1021,6 +1022,7 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int pager=0x7f05003d;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1041,6 +1043,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int textMessage=0x7f05003f;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1097,11 +1100,21 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int activity_main=0x7f030017;
-        public static final int fragment_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_messaging=0x7f030018;
+        public static final int activity_music=0x7f030019;
+        public static final int activity_phone_call=0x7f03001a;
+        public static final int fragment_main=0x7f03001b;
+        public static final int fragment_messaging=0x7f03001c;
+        public static final int fragment_music=0x7f03001d;
+        public static final int fragment_phone_call=0x7f03001e;
+        public static final int page=0x7f03001f;
+        public static final int support_simple_spinner_dropdown_item=0x7f030020;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
+        public static final int messaging=0x7f0c0001;
+        public static final int music=0x7f0c0002;
+        public static final int phone_call=0x7f0c0003;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1147,6 +1160,18 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int listen_music=0x7f0a001b;
+        public static final int next=0x7f0a0013;
+        public static final int phone_call=0x7f0a0012;
+        public static final int prev=0x7f0a0014;
+        public static final int start=0x7f0a0010;
+        public static final int title_activity_messaging=0x7f0a0016;
+        public static final int title_activity_music=0x7f0a001a;
+        public static final int title_activity_phone_call=0x7f0a0011;
+        public static final int title_section1=0x7f0a0017;
+        public static final int title_section2=0x7f0a0018;
+        public static final int title_section3=0x7f0a0019;
+        public static final int view_message=0x7f0a0015;
     }
     public static final class style {
         /** 
--- Broid_MK-I/src/iitd/ee/ms/broid/MainActivity.java
+++ Broid_MK-I/src/iitd/ee/ms/broid/MainActivity.java
@@ -1,14 +1,17 @@
 package iitd.ee.ms.broid;
 
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
@@ -17,11 +20,15 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		
+		
 	}
 
 	@Override
@@ -59,6 +66,11 @@ public class MainActivity extends ActionBarActivity {
 					false);
 			return rootView;
 		}
+	}
+	
+	public void startApplication(View view){
+		Intent intent = new Intent(this, PhoneCallActivity.class);
+		startActivity(intent);
 	}
 
 }
